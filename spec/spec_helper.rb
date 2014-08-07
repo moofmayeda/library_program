f48851d04d66b4c3f2de2a1b70cf78debafb8604
moof mayeda & Nick Bernard <student@epicodus.com>
2014-08-07 09:47:55 -0700

    initial commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+require 'pg'
+require 'rspec'
+require 'book'
+require 'patron'
+
+DB = PG.connect({:dbname => 'library_test'})
+
+RSpec.configure do |config|
+  config.before(:each) do
+    DB.exec("DELETE FROM books *;")
+    DB.exec("DELETE FROM authors *;")
+    DB.exec("DELETE FROM books_authors *;")
+    DB.exec("DELETE FROM patrons *;")
+    DB.exec("DELETE FROM copies *;")
+    DB.exec("DELETE FROM checkouts *;")
+    DB.exec("DELETE FROM returns *;")
+  end
+end
